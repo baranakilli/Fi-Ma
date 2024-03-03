94f20208bf843c2e87ed04b732e52e5a0ba1af25
Baran Akıllı <akillibaran@hotmail.com>
2024-03-04 01:18:16 +0300

    Finalized

--- functions/index.js
+++ functions/index.js
@@ -2,25 +2,26 @@ const {onRequest} = require("firebase-functions/v2/https");
 const cors = require("cors")({origin: true});
 const fetch = require("node-fetch");
 
-exports.getTrendyolProducts = onRequest((request, response) => {
-  cors(request, response, () => {
-    const url = "https://api.trendyol.com/sapigw/suppliers/645403/products?onSale=true&size=9999";
+exports.getTrendyolProducts = onRequest(
+    {secrets: ["REACT_APP_API_TOKEN"]}, (request, response) => {
+      cors(request, response, () => {
+        const url = "https://api.trendyol.com/sapigw/suppliers/645403/products?onSale=true&size=9999";
 
-    fetch(url, {
-      method: "get",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Basic ${process.env.REACT_APP_API_TOKEN}`,
-        "User-Agent": "645403 - SelfIntegration",
-      },
-    })
-        .then((res) => res.json())
-        .then((data) => {
-          response.json(data);
+        fetch(url, {
+          method: "get",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Basic ${process.env.REACT_APP_API_TOKEN}`,
+            "User-Agent": "645403 - SelfIntegration",
+          },
         })
-        .catch((err) => {
-          console.error(err);
-          response.status(500).send("Error");
-        });
-  });
-});
+            .then((res) => res.json())
+            .then((data) => {
+              response.json(data);
+            })
+            .catch((err) => {
+              console.error(err);
+              response.status(500).send("Error");
+            });
+      });
+    });
