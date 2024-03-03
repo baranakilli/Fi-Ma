3b09cbeedebb1c70bb729bc6754078558e780606
Baran Akıllı <akillibaran@hotmail.com>
2024-03-03 20:03:13 +0300

    updated

--- src/Components/ProductCard/ProductCard.js
+++ src/Components/ProductCard/ProductCard.js
@@ -1,17 +1,19 @@
 import './ProductCard.css';
 
-const ProductCard = ({title, imageUrl, onClickLink}) => {
-
+const ProductCard = ({ title, imageUrl, onClickLink }) => {
   const redirectToSite = () => {
     window.location.href = onClickLink;
   };
 
   return (
-    <div className="product-card bg-white dib br3 pa1 ma2 grow ba bw1 b--light-gray shadow-5 justify-center w-10" onClick={redirectToSite}>
-      <img className='product-image' src={imageUrl} alt="product"/>
-      <h6 className='product-title'>{title}</h6>
+    <div
+      className="product-card bg-white dib br3 pa1 ma2 grow ba bw1 b--light-gray shadow-5 justify-center w-10"
+      onClick={redirectToSite}
+    >
+      <img className="product-image" src={imageUrl} alt="product" />
+      <h6 className="product-title">{title}</h6>
     </div>
   );
-}
+};
 
 export default ProductCard;
