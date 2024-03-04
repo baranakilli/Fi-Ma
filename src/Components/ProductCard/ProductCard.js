08c3a08c00aa8be1782bb703746dc10edc2b0b85
Baran Akıllı <akillibaran@hotmail.com>
2024-03-04 17:55:31 +0300

    changed product link targets

--- src/Components/ProductCard/ProductCard.js
+++ src/Components/ProductCard/ProductCard.js
@@ -1,14 +1,10 @@
 import './ProductCard.css';
 
-const ProductCard = ({ title, imageUrl, onClickLink }) => {
-  const redirectToSite = () => {
-    window.location.href = onClickLink;
-  };
+const ProductCard = ({ title, imageUrl }) => {
 
   return (
     <div
       className="product-card bg-white dib br3 pa1 ma2 grow ba bw1 b--light-gray shadow-5 justify-center w-10"
-      onClick={redirectToSite}
     >
       <img className="product-image" src={imageUrl} alt="product" />
       <h6 className="product-title">{title}</h6>
