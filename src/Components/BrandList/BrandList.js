3949ae3df95110ace5bc4b30f10aaaced9f83930
Baran Akıllı <akillibaran@hotmail.com>
2024-05-24 03:04:05 +0300

    Fixed deployment errors

--- src/Components/BrandList/BrandList.js
+++ src/Components/BrandList/BrandList.js
@@ -1,29 +1,29 @@
 import './BrandList.css';
 
-const scrollToLeft = () => {
-  const brandsContainer = document.getElementById('brands-container');
-  const groupWidth = brandsContainer.offsetWidth;
-  const currentScrollLeft = brandsContainer.scrollLeft;
-  if (currentScrollLeft !== 0 || currentScrollLeft % groupWidth === 0) {
-    brandsContainer.scrollBy({ left: -groupWidth, behavior: 'smooth' });
-  }
-};
+// const scrollToLeft = () => {
+//   const brandsContainer = document.getElementById('brands-container');
+//   const groupWidth = brandsContainer.offsetWidth;
+//   const currentScrollLeft = brandsContainer.scrollLeft;
+//   if (currentScrollLeft !== 0 || currentScrollLeft % groupWidth === 0) {
+//     brandsContainer.scrollBy({ left: -groupWidth, behavior: 'smooth' });
+//   }
+// };
 
-const scrollToRight = () => {
-  const brandsContainer = document.getElementById('brands-container');
-  const groupWidth = brandsContainer.offsetWidth;
-  const currentScrollLeft = brandsContainer.scrollLeft;
-  const maxScrollWidth = brandsContainer.scrollWidth;
-  console.log(groupWidth);
-  console.log(currentScrollLeft);
-  if (currentScrollLeft === 0 || currentScrollLeft % groupWidth === 0) {
-    if (currentScrollLeft + groupWidth + 10 >= maxScrollWidth) {
-      brandsContainer.scrollTo({ left: 0, behavior: 'smooth' });
-    } else {
-      brandsContainer.scrollBy({ left: groupWidth, behavior: 'smooth' });
-    }
-  }
-};
+// const scrollToRight = () => {
+//   const brandsContainer = document.getElementById('brands-container');
+//   const groupWidth = brandsContainer.offsetWidth;
+//   const currentScrollLeft = brandsContainer.scrollLeft;
+//   const maxScrollWidth = brandsContainer.scrollWidth;
+//   console.log(groupWidth);
+//   console.log(currentScrollLeft);
+//   if (currentScrollLeft === 0 || currentScrollLeft % groupWidth === 0) {
+//     if (currentScrollLeft + groupWidth + 10 >= maxScrollWidth) {
+//       brandsContainer.scrollTo({ left: 0, behavior: 'smooth' });
+//     } else {
+//       brandsContainer.scrollBy({ left: groupWidth, behavior: 'smooth' });
+//     }
+//   }
+// };
 
 const BrandList = () => {
   return (
